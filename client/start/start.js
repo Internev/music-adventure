bbdbdaa79f405d3fb97ad814e099995372562630
Neville Neville <nchallinger@gmail.com>
2016-12-07 09:01:29 +1100

    Create playlist option, populated randomly from top artist tracks

--- client/start/start.js
+++ client/start/start.js
@@ -1,7 +1,7 @@
 
 angular.module('start', ['youtube-embed', 'util'])
 
-.controller('StartCtrl', function($http, Util) {
+.controller('StartCtrl', function($http, Util, $scope) {
 
   this.artistData = {};
   this.relatedArtists = [];
@@ -12,6 +12,18 @@ angular.module('start', ['youtube-embed', 'util'])
     controls: 1,
     autoplay: 1
   }
+
+  $scope.$on('youtube.player.ended', ($event, player)=>{
+    if (this.playList.length > 0){
+      Util.getYoutubeData(this.playList.pop())
+      .then((resp) => {
+        this.youtubeUrl = resp.data.items[0].id.videoId;
+        player.playVideo();
+      });
+    } else {
+      this.getData(this.artistData.name);
+    }
+  });
 
 //I have created promisecallbackhell?!?
   this.getData = function(name){
@@ -26,22 +38,35 @@ angular.module('start', ['youtube-embed', 'util'])
             this.artistSongs = resp.data.toptracks.track;
             var track = Math.floor(Math.random() * this.artistSongs.length);
             this.playList.push(name + '-' + resp.data.toptracks.track[track].name);
-            console.log(this.playList);
+            console.log('playlist after first call to songdata:', this.playList);
           })
           .then(()=>{
             Util.getYoutubeData(this.playList.pop())
             .then((resp) => {
               console.log('youtube:', resp);
-              this.youtubeUrl = resp.data.items[0].id.videoId
+              this.youtubeUrl = resp.data.items[0].id.videoId;
             });
           })
       })
-      .then(this.populatePlaylist);
   }
 
-
-  this.populatePlaylist = function(){
-    
+  this.populatePlaylist = function(name){
+    Util.getLastfmSongData(name)
+      .then((resp) => {
+        this.artistSongs = resp.data.toptracks.track;
+        var track = Math.floor(Math.random() * this.artistSongs.length);
+        this.playList.push(name + '-' + resp.data.toptracks.track[track].name);
+        console.log('Playlist changed, now:', this.playList);
+      })
   }
+
+  // Request tracks every 5 seconds so we don't flood API.
+  setInterval(()=>{
+    if (this.relatedArtists.length > 0){
+      var artist = this.relatedArtists.pop();
+      console.log('about to populate playlist with:', artist.name);
+      this.populatePlaylist(artist.name);
+    }
+  }, 5000);
 
 });
