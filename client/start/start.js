796c778e03ef1a68d3ba362f1b298588f513f1d6
Neville Neville <nchallinger@gmail.com>
2016-12-06 16:45:16 +1100

    Add basic style. Split API calls into separate promises

--- client/start/start.js
+++ client/start/start.js
@@ -26,20 +26,4 @@ angular.module('start', ['youtube-embed', 'util'])
       });
 
   }
-
-  // this.getData = function(name){
-  //   $http({
-  //       method: 'GET',
-  //       url: '/lastfm',
-  //       params: {name: name}
-  //   })
-  //   .then((resp) => {
-  //     console.log(resp.data.youtube.items[0]);
-  //     this.artistData = resp.data.lastfm.artist;
-  //     this.relatedArtists = resp.data.lastfm.artist.similar.artist;
-  //     this.youtubeUrl = resp.data.youtube.items[0].id.videoId;
-  //     console.log(this.youtubeUrl);
-  //   });
-  // }
-
 });
